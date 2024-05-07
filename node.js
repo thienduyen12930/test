ab3ac41fb152625ed182deaef4c0ed170e45a2f9
thienduyen12930 <144190277+thienduyen12930@users.noreply.github.com>
2024-05-07 20:05:13 +0700

    Update node.js

--- node.js
+++ node.js
@@ -2,7 +2,7 @@ const http = require('http')
 const port = '3000'
 const host = '127.0.0.1'
 const server = http.createServer((req, res) => {
-    res.end('Hello FPT UNiversity University')
+    res.end('Hello sasa')
 })
 server.listen(port,host,()=>{
     console.log(`Server running at http://${host}:${port}`)
